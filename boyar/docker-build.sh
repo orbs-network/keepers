159c3b4e1ac18e39b68a6036e7309b73d72c9d4d
uvorbs <yuval@orbs.com>
2022-07-05 17:36:19 +0300

    remove --

--- boyar/docker-build.sh
+++ boyar/docker-build.sh
@@ -2,4 +2,4 @@
 
 npm run build
 #docker build -t orbsnetworkstaging/keepers:$(cat .version) ./boyar
---docker buildx build --platform linux/x86-64 --no-cache -t orbsnetworkstaging/keepers:$(cat .version) --push ./boyar
+docker buildx build --platform linux/x86-64 --no-cache -t orbsnetworkstaging/keepers:$(cat .version) --push ./boyar
