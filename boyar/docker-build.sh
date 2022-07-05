4fbecfe78643790222f130620f6dcc1e28df0dfb
uvorbs <yuval@orbs.com>
2022-07-05 15:51:52 +0300

    buildx small socker

--- boyar/docker-build.sh
+++ boyar/docker-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 npm run build
-docker build -t orbsnetworkstaging/keepers:$(cat .version) ./boyar
+#docker build -t orbsnetworkstaging/keepers:$(cat .version) ./boyar
+--docker buildx build --platform linux/x86-64 --no-cache -t orbsnetworkstaging/keepers:$(cat .version) --push ./boyar
