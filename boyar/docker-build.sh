c529b45068e663ffb3fbb875e8b8c923ec9afe8a
uvorbs <yuval@orbs.com>
2022-07-05 17:24:02 +0300

    usign FROM --platform docker

--- boyar/docker-build.sh
+++ boyar/docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 npm run build
-#docker build -t orbsnetworkstaging/keepers:$(cat .version) ./boyar
---docker buildx build --platform linux/x86-64 --no-cache -t orbsnetworkstaging/keepers:$(cat .version) --push ./boyar
+docker build -t orbsnetworkstaging/keepers:$(cat .version) ./boyar
+#--docker buildx build --platform linux/x86-64 --no-cache -t orbsnetworkstaging/keepers:$(cat .version) --push ./boyar
